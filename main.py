d2ba2f802fb2944ebf9d50b0cfd011db38cc9cd8
BaekInHyeok <bih63879@gmail.com>
2023-12-27 22:19:36 +0900

    일부 수정

--- main.py
+++ main.py
@@ -1,32 +1,31 @@
 import threading
 import time
-import RPi.GPIO as GPIO
-import speech_recognition as sr
+# import speech_recognition as sr
 
-#main.py와 연결되는 파일들
+
 import display
 import ai
 import waterlevel
 
 def main():
     
-    global case #case를 전역변수로 사용하겠다고 선언
+    global case 
     
     while True:
-        # 물 높이 측정 스레드 생성 및 시작
+        
         waterlevel_thread = threading.Thread(target=waterlevel.waterlevelmeasure, args=(case.lock,))
         waterlevel_thread.start()
             
-        # AI 호출 스레드 생성 및 시작
-        with sr.Microphone() as source:
-            ai_thread = threading.Thread(target=ai.listen_for_wake_word, args=(source,case.lock))
-            ai_thread.start()
         
-        # 두 스레드가 종료될 때까지 기다림
+        # with sr.Microphone() as source:
+        #     ai_thread = threading.Thread(target=ai.listen_for_wake_word, args=(source,case.lock))
+        #     ai_thread.start()
+        
+        
         waterlevel_thread.join()   
-        ai_thread.join()
+        # ai_thread.join()
         
-        # 여기에서 case 값에 따라 적절한 작업 수행
+        
         display.show_gif(case)
         
 if __name__ == "__main__":
--- waterlevel.py
+++ waterlevel.py
@@ -1,7 +1,7 @@
 import RPi.GPIO as GPIO
 import spidev
 
-water_level = 0 #물높이 센서를 ADC 0번 채널에 연결한다.
+water_level = 0
 
 global case
 case=None
@@ -11,12 +11,9 @@ def waterlevelmeasure(lock):
     
     global case
     
-    # MCP3208 SPI 버스 및 디바이스 설정
     spi = spidev.SpiDev()
     spi.open(0,0)
 
-    
-    # MCP3208에서 아날로그 값을 읽어와 반환
     data = 0
     adc = spi.xfer2([1, (8 + water_level) << 4, 0])
     data=((adc[1] & 3) << 8) + adc[2]
@@ -24,9 +21,9 @@ def waterlevelmeasure(lock):
     print(data)
     
     with lock:
-        if data > high_threshold:  # Check water level against high threshold
+        if data > 10000:  # Check water level against high threshold
             case = 1  # High water level condition
-        elif data > medium_threshold and data <= high_threshold:  # Check water level against medium threshold
+        elif data > 5000 and data <= 10000:  # Check water level against medium threshold
             case = 2  # Medium water level condition
         else :  # Check water level against low threshold
             case = 3  # Low water level condition
